bbdf5cc853f1a68258d47c6f4f00bdd5d69a1fe6
mi12205599 <hiAladdin@163.com>
2016-01-10 01:24:16 +0800

    merge

--- QuickBootstrap/Controllers/ReportController.cs
+++ QuickBootstrap/Controllers/ReportController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuickBootstrap.Filters;
 
 namespace QuickBootstrap.Controllers
 {
+    [UserAuthorization]
     public class ReportController : Controller
     {
         // GET: Report
